5283e0ae04251bd3c39d46c82d7cf9574d5775fe
David Mente <davidmente@hotmail.de>
2019-12-22 09:30:34 +0100

    debounces search filter

--- src/client/src/components/build_orders/buildOrdersSearch.tsx
+++ src/client/src/components/build_orders/buildOrdersSearch.tsx
@@ -2,6 +2,7 @@ import React, {FunctionComponent} from 'react';
 import {RootState} from '../../store';
 import {connect, ConnectedProps} from 'react-redux';
 import {setBuildOrdersSearch} from '../../store/build_orders/actions';
+import {DebounceInput} from 'react-debounce-input';
 
 const mapState = (state: RootState) => {
     return {
@@ -17,8 +18,8 @@ const connector = connect(mapState, mapDispatch);
 
 const BuildOrdersSearch: FunctionComponent<ConnectedProps<typeof connector>> = ({search, filter}) =>
     <div className='field'>
-        <input className={'input'} value={search || ''} placeholder={'Search'}
-               onChange={(event: React.ChangeEvent<HTMLInputElement>) => filter(event.target.value)}/>
+        <DebounceInput className={'input'} value={search || ''} placeholder={'Search'} debounceTimeout={300}
+                       onChange={(event: React.ChangeEvent<HTMLInputElement>) => filter(event.target.value)}/>
     </div>;
 
 export default connector(BuildOrdersSearch);
